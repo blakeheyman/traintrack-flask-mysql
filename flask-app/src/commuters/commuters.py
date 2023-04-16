48baa158e49b6858f0fb33446bf1b171ee685d98
Blake Heyman <blake.heyman@gmail.com>
2023-04-16 18:49:40 -0400

    First few commuter endpoints

--- flask-app/src/commuters/commuters.py
+++ flask-app/src/commuters/commuters.py
@@ -6,7 +6,7 @@ from src import db
 commuters = Blueprint('commuters', __name__)
 
 # Get all of a user's favorites
-@commuters.route('/commuters/<email>/favorites')
+@commuters.route('/<email>/favorites', methods=['GET'])
 def get_favorites(email):
     cursor = db.get_db().cursor()
     query = '''SELECT favorites.nickname, stops.id, stops.location_name, routes.name 
@@ -21,58 +21,64 @@ def get_favorites(email):
     theData = cursor.fetchall()
     for row in theData:
         json_data.append(dict(zip(column_headers, row)))
+
+    if len(json_data) == 0:
+        return jsonify({'message': 'No favorites found for this user.'}), 404
     return jsonify(json_data)
 
-# # Get all the commuters from the database
-# @products.route('/products', methods=['GET'])
-# def get_products():
-#     # get a cursor object from the database
-#     cursor = db.get_db().cursor()
+# Add a favorite to a user's favorites
+@commuters.route('/<email>/favorites', methods=['POST'])
+def add_favorite(email):
+    cursor = db.get_db().cursor()
+    query = '''INSERT INTO favorites (com_email, stop_id, nickname) 
+        VALUES (\'{0}\', {1}, \'{2}\')
+    '''.format(email, request.json['stop_id'], request.json['nickname'])
+    try:
+        cursor.execute(query)
+        db.get_db().commit()
+        return jsonify({'message': 'Favorite added successfully.'}), 201
+    except:
+        return jsonify({'message': 'Favorite already exists.'}), 409
 
-#     # use cursor to query the database for a list of products
-#     cursor.execute('SELECT id, product_code, product_name, list_price FROM products')
+# Delete a favorite from a user's favorites
+@commuters.route('/<email>/favorites/<stop_id>', methods=['DELETE'])
+def delete_favorite(email, stop_id):
+    cursor = db.get_db().cursor()
+    query = '''DELETE FROM favorites 
+        WHERE STRCMP(com_email, \'{0}\') = 0 AND stop_id = {1}
+    '''.format(email, stop_id)
+    cursor.execute(query)
+    db.get_db().commit()
+    if cursor.rowcount == 1:
+        return jsonify({'message': 'Favorite deleted successfully.'}), 200
+    else:
+        return jsonify({'message': 'Favorite does not exist.'}), 404
 
-#     # grab the column headers from the returned data
-#     column_headers = [x[0] for x in cursor.description]
+# Update the nickname of a favorite
+@commuters.route('/<email>/favorites/<stop_id>', methods=['PUT'])
+def update_favorite(email, stop_id):
+    cursor = db.get_db().cursor()
+    query = '''UPDATE favorites 
+        SET nickname = \'{0}\' 
+        WHERE STRCMP(com_email, \'{1}\') = 0 AND stop_id = {2}
+    '''.format(request.json['nickname'], email, stop_id)
+    cursor.execute(query)
+    db.get_db().commit()
+    if cursor.rowcount == 1:
+        return jsonify({'message': 'Nickname updated successfully.'}), 200
+    else:
+        return jsonify({'message': 'Nickname was not updated.'}), 400
 
-#     # create an empty dictionary object to use in 
-#     # putting column headers together with data
-#     json_data = []
-
-#     # fetch all the data from the cursor
-#     theData = cursor.fetchall()
-
-#     # for each of the rows, zip the data elements together with
-#     # the column headers. 
-#     for row in theData:
-#         json_data.append(dict(zip(column_headers, row)))
-
-#     return jsonify(json_data)
-
-# # get the top 5 products from the database
-# @products.route('/mostExpensive')
-# def get_most_pop_products():
-#     cursor = db.get_db().cursor()
-#     query = '''
-#         SELECT product_code, product_name, list_price, reorder_level
-#         FROM products
-#         ORDER BY list_price DESC
-#         LIMIT 5
-#     '''
-#     cursor.execute(query)
-#        # grab the column headers from the returned data
-#     column_headers = [x[0] for x in cursor.description]
-
-#     # create an empty dictionary object to use in 
-#     # putting column headers together with data
-#     json_data = []
-
-#     # fetch all the data from the cursor
-#     theData = cursor.fetchall()
-
-#     # for each of the rows, zip the data elements together with
-#     # the column headers. 
-#     for row in theData:
-#         json_data.append(dict(zip(column_headers, row)))
-
-#     return jsonify(json_data)
+# Submit a report for a problem
+@commuters.route('/reports', methods=['POST'])
+def submit_report():
+    cursor = db.get_db().cursor()
+    query = '''INSERT INTO reports (message, commuter_email) 
+        VALUES (\'{0}\', \'{1}\')
+    '''.format(request.json['message'], request.json['email'])
+    try:
+        cursor.execute(query)
+        db.get_db().commit()
+        return jsonify({'message': 'Report submitted successfully.'}), 201
+    except:
+        return jsonify({'message': 'Report was not submitted.'}), 400
